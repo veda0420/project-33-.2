4f21ddd68660d6c3dab3e7d72c7eae700cf1b5bf
veda0420 <75167659+veda0420@users.noreply.github.com>
2022-06-03 14:38:03 +0530

    Update game.js

--- js/game.js
+++ js/game.js
@@ -63,7 +63,11 @@ class Game{
 
                          
                      }
-                    
+                    textSize(25);
+                     fill("white");
+                     text("Player 1 :" +allPlayers.player1.score,50,50);
+                    text("Player 2 :" + allPlayers.player2.score, 50, 100);
+                     
                      
                  
                  }
